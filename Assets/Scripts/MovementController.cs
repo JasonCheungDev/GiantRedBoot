69e4f9937f9e1a7ca991e701514dc99d8efb3022
daniel <daniel_tian2002@hotmail.com>
2017-10-04 15:15:58 -0800

    added movement controllers

--- Assets/Scripts/MovementController.cs
+++ Assets/Scripts/MovementController.cs
@@ -1,16 +1,30 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class MovementController : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    public float speed = 5.0f;
+
+    void Update()
+    {
+        transform.Translate(Input.GetAxisRaw("Horizontal") * speed * Time.deltaTime, Input.GetAxisRaw("Vertical") * speed * Time.deltaTime, 0);
+        if (Input.GetButtonDown("Fire1"))
+        {
+            ClickTransform();
+        }
+    }
+
+    void ClickTransform()
+    {
+        RaycastHit2D hit = Physics2D.Raycast(Camera.main.ScreenToWorldPoint(Input.mousePosition), Vector2.zero);
+        //Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+       if (hit.collider != null)
+       {
+            print(hit.collider.name);
+            //print(mousePos.x + " y: " + mousePos.y);
+            //Debug.Log("Target Position: " + hit.collider.gameObject.transform.position);
+        }
+    }
 }
+
+//float xVel = Input.GetAxisRaw("Horizontal");
+//float yVel = Input.GetAxisRaw("Vertical");
